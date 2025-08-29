44365273c72639e6dd1a714811cef9f125f9ebd4
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-08-29 10:56:58 +0200

    Added logging and settings

---
+++ mosamatic2/src/mosamatic2/ui/settings.py
@@ -0,0 +1,62 @@
+import mosamatic2.constants as constants
+from PySide6.QtCore import QSettings
+from mosamatic2.core.utils.logmanager import LogManager
+
+LOG = LogManager()
+
+
+class Settings(QSettings):
+    def __init__(self):
+        super(Settings, self).__init__(
+            QSettings.IniFormat, 
+            QSettings.UserScope, 
+            constants.MOSAMATIC2_BUNDLE_IDENTIFIER, 
+            constants.MOSAMATIC2_APP_NAME,
+        )
+
+    def prepend_bundle_identifier_and_name(self, name):
+        return '{}.{}.{}'.format(constants.MOSAMATIC2_BUNDLE_IDENTIFIER, constants.MOSAMATIC2_APP_NAME, name)
+
+    def get(self, name, default=None):
+        if not name.startswith(constants.MOSAMATIC2_BUNDLE_IDENTIFIER):
+            name = self.prepend_bundle_identifier_and_name(name)
+        value = self.value(name)
+        if value is None or value == '':
+            return default
+        return value
+    
+    def get_int(self, name, default=None):
+        try:
+            return int(self.get(name, default))
+        except ValueError as e:
+            return default
+        
+    def get_float(self, name, default=None):
+        try:
+            return float(self.get(name, default))
+        except ValueError as e:
+            return default
+        
+    def get_bool(self, name, default=None):
+        try:
+            value = self.get(name, default)
+            if value and isinstance(value, str):
+                if value == '0' or value.lower() == 'false':
+                    return False
+                elif value == '1' or value.lower() == 'true':
+                    return True
+                else:
+                    return default
+            if value and isinstance(value, bool):
+                return value
+        except ValueError as e:
+            return default
+    
+    def set(self, name, value):
+        name = self.prepend_bundle_identifier_and_name(name)
+        self.setValue(name, value)
+
+    def print(self):
+        LOG.info(f'Settings path: {self.fileName()}')
+        for key in self.allKeys():
+            LOG.info(f'Settings: {key}')
