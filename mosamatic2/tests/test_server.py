938582a74bbe67aa819df924908b48178dfd8a32
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-08-31 16:12:37 +0200

    Disabled server test

--- mosamatic2/tests/test_server.py
+++ mosamatic2/tests/test_server.py
@@ -1,6 +1,6 @@
-import requests
+# import requests
 
 
-def test_server():
-    result = requests.get('http://localhost:8000/test')
-    assert result.text == 'PASSED'
+# def test_server():
+#     result = requests.get('http://localhost:8000/test')
+#     assert result.text == 'PASSED'
