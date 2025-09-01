7c6812481d08fb22f010fb7ca5c295ce49383592
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-09-01 14:12:39 +0200

    No need for DICOM decompression working on rescale DICOM images

--- mosamatic2/src/mosamatic2/core/tasks/rescaledicomimagestask/rescaledicomimagestask.py
+++ mosamatic2/src/mosamatic2/core/tasks/rescaledicomimagestask/rescaledicomimagestask.py
@@ -3,9 +3,9 @@ import shutil
 from mosamatic2.core.tasks.task import Task
 
 
-class DecompressDicomImagesTask(Task):
+class RescaleDicomImagesTask(Task):
     def __init__(self, inputs, output, params):
-        super(DecompressDicomImagesTask, self).__init__(inputs, output, params)
+        super(RescaleDicomImagesTask, self).__init__(inputs, output, params)
 
     def run(self):
         pass
