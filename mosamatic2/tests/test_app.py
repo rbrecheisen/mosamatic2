deecac8e217d883fc24db3067b8ffd83c672c133
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-09-02 10:49:43 +0200

    Reformatted tests directory

--- mosamatic2/tests/test_app.py
+++
@@ -1,5 +0,0 @@
-from mosamatic2.app import run_tests
-
-
-def test_app():
-    assert run_tests() == 'PASSED'
--- mosamatic2/tests/test_server.py
+++
@@ -1,6 +0,0 @@
-# import requests
-
-
-# def test_server():
-#     result = requests.get('http://localhost:8000/test')
-#     assert result.text == 'PASSED'
