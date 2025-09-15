134347a5953932de344d6d0941cef2995cf45f17
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-09-15 13:06:56 +0200

    Saving work

--- mosamatic2/src/mosamatic2/ui/widgets/panels/visualizations/niftislicevisualization/niftislicevisualization.py
+++ mosamatic2/src/mosamatic2/ui/widgets/panels/visualizations/niftislicevisualization/niftislicevisualization.py
@@ -1,14 +1,14 @@
-from mosamatic2.ui.widgets.panels.visualizations.dicomslicevisualization.multistyleinteractor import MultiStyleInteractor
+from mosamatic2.ui.widgets.panels.visualizations.niftislicevisualization.multistyleinteractor import MultiStyleInteractor
 from mosamatic2.ui.widgets.panels.visualizations.visualization import Visualization
 
 
-class DicomSliceVisualization(Visualization):
+class NiftiSliceVisualization(Visualization):
     """
-    DicomSliceVisualization
+    NiftiSliceVisualization
 
     This visualization can load a single NIFTI volume and display it as a stack of slices
     through which you can scroll. You can also add a mask volume, possibly with multiple
     labels, that will be shown as an overlay.
     """
     def __init__(self):
-        super(DicomSliceVisualization, self).__init__()
+        super(NiftiSliceVisualization, self).__init__()
