b61f3092a3b184fba8b4fe8cc28e003f93d89f28
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-09-29 09:35:08 +0200

    Deployed version 2.0.11 (with Docker support)

--- mosamatic2/scripts/deploy2pypi.bat
+++ mosamatic2/scripts/deploy2pypi.bat
@@ -34,13 +34,12 @@ set /p TOKEN=<"G:\My Drive\data\ApiKeysAndPasswordFiles\pypi-token.txt"
 
 poetry publish --build --username __token__ --password %TOKEN%
 
-set /p CONFIRM="Do you also want to build and deploy a Docker image of mosamatic2? (y/n) "
-if /I NOT "%CONFIRM%"=="y" (
-    echo Aborting deployment
-    exit /b 1
-)
-
-call scripts\builddockerimage.bat
-call scripts\deploy2dockerhub.bat
+@REM set /p CONFIRM="Do you also want to build and deploy a Docker image of mosamatic2? (y/n) "
+@REM if /I NOT "%CONFIRM%"=="y" (
+@REM     echo Aborting deployment
+@REM     exit /b 1
+@REM )
+@REM call scripts\builddockerimage.bat
+@REM call scripts\deploy2dockerhub.bat
 
 endlocal
---
+++ mosamatic2/tests/pipelines/test_defaultdockerpipeline.py
@@ -0,0 +1,58 @@
+import os
+
+from mosamatic2.core.pipelines import DefaultDockerPipeline
+from mosamatic2.core.utils import is_dicom
+from tests.sources import get_sources
+
+SOURCES = get_sources()
+
+
+def test_default_docker_pipeline():
+    assert os.path.exists(SOURCES['input']), 'Input directory does not exist'
+    pipeline = DefaultDockerPipeline(
+        inputs={
+            'images': SOURCES['input'],
+            'model_files': SOURCES['model_files']['tensorflow'],
+        },
+        params={
+            'target_size': 512,
+            'model_type': 'tensorflow',
+            'model_version': 1.0,
+            'file_type': 'npy',
+            'fig_width': 10,
+            'fig_height': 10,
+            'version': '2.0.10',
+        },
+        output=SOURCES['output'],
+        overwrite=True,
+    )
+    pipeline.run()
+    check_output(pipeline)
+
+
+def check_output(pipeline):
+
+    output_dir = os.path.join(pipeline.output(), 'defaultpipeline/rescaledicomimagestask')
+    assert os.path.exists(output_dir), 'Output directory does not exist'
+    assert len(os.listdir(output_dir)) == 4, 'Output directory does not contain 4 files'
+    for f in os.listdir(output_dir):
+        assert is_dicom(os.path.join(output_dir, f)), f'File {f} is not DICOM'
+
+    task_name = 'segmentmusclefatl3pytorchtask' if pipeline.param('model_type') == 'pytorch' else 'segmentmusclefatl3tensorflowtask'
+    output_dir = os.path.join(pipeline.output(), 'defaultpipeline/' + task_name)
+    assert os.path.exists(output_dir), 'Output directory does not exist'
+    assert len(os.listdir(output_dir)) == 4, 'Output directory does not contain 4 files'
+    for f in os.listdir(output_dir):
+        assert f.endswith('.seg.npy'), f'File {f} is not a NumPy file'
+
+    output_dir = os.path.join(pipeline.output(), 'defaultpipeline/calculatescorestask')
+    assert os.path.exists(output_dir), 'Output directory does not exist'
+    assert len(os.listdir(output_dir)) == 2, 'Output directory does not contain 2 files'
+    assert os.path.exists(os.path.join(output_dir, 'bc_scores.csv'))
+    assert os.path.exists(os.path.join(output_dir, 'bc_scores.xlsx'))
+
+    output_dir = os.path.join(pipeline.output(), 'defaultpipeline/createpngsfromsegmentationstask')
+    assert os.path.exists(output_dir), 'Output directory does not exist'
+    assert len(os.listdir(output_dir)) == 4, 'Output directory does not contain 4 files'
+    for f in os.listdir(output_dir):
+        assert f.endswith('.seg.npy.png'), f'File {f} is not a PNG file'
