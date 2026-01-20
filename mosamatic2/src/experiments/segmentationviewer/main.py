6f8dc963fc19f0a433bc144f8584c67c17bed27f
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2026-01-20 13:53:57 +0100

    Added sliders for both low and high RA

--- mosamatic2/src/experiments/segmentationviewer/main.py
+++ mosamatic2/src/experiments/segmentationviewer/main.py
@@ -13,16 +13,21 @@ SEGMENTATION_FILE_NAME = '04RAND092.dcm.seg.npy'
 SEGMENTATION_FILE_PATH = f'/Users/ralph/Library/CloudStorage/GoogleDrive-ralph.brecheisen@gmail.com/My Drive/data/mosamatic2/data/l3_edema/{SEGMENTATION_FILE_NAME}'
 if sys.platform.startswith('win'):
     SEGMENTATION_FILE_PATH = f'G:\\My Drive\\data\\mosamatic2\\data\\l3_edema\\{SEGMENTATION_FILE_NAME}'
+HU_MIN = -29
+HU_MAX = 150
 
-slider_ax = None
-slider = None
+slider_lo_ax = None
+slider_hi_ax = None
+slider_lo = None
+slider_hi = None
 image = None
 muscle_mask = None
 hu_lo = 30
 hu_hi = 150
 fig = None
 axes = None
-slider_value = None
+slider_lo_text = None
+slider_hi_text = None
 
 
 def rgba_from_binary_mask(mask, rgba, alpha):
@@ -35,26 +40,32 @@ def rgba_from_binary_mask_thresholded(image, mask, label, hu_lo, hu_hi, alpha):
     mask = (mask == label)
     red = mask & (image >= hu_lo) & (image <= hu_hi)
     yellow = mask & (image < hu_lo)
-    # red = mask & (image <= hu_lo) & (image >= hu_hi)
-    # yellow = mask & (image > hu_lo) & (image < hu_hi)
     overlay = np.zeros((*image.shape, 4), dtype=np.float32)
     overlay[red] = (1.0, 0.0, 0.0, alpha)
     overlay[yellow] = (1.0, 1.0, 0.0, alpha)
     return overlay
 
 
-def on_change(value):
+def on_change_lo(value):
     axes[1].clear()
     axes[1].imshow(image, cmap="gray")
     axes[1].imshow(rgba_from_binary_mask_thresholded(image, muscle_mask, label=1, hu_lo=value, hu_hi=hu_hi, alpha=1))
     axes[1].axis("off")
-    axes[1].set_title(f"Muscle HU overlay")
-    slider_value.set_text(f"Threshold: {value:.1f}")
+    axes[1].set_title(f"Muscle low-RA (yellow) and high-RA (red)")
+    fig.canvas.draw_idle()
+
+
+def on_change_hi(value):
+    axes[1].clear()
+    axes[1].imshow(image, cmap="gray")
+    axes[1].imshow(rgba_from_binary_mask_thresholded(image, muscle_mask, label=1, hu_lo=hu_lo, hu_hi=value, alpha=1))
+    axes[1].axis("off")
+    axes[1].set_title(f"Muscle low-RA (yellow) and high-RA (red)")
     fig.canvas.draw_idle()
 
 
 def main():
-    global fig, axes, image, muscle_mask, slider, slider_ax, slider_value
+    global fig, axes, image, muscle_mask, slider_lo, slider_lo_ax, slider_lo_text
 
     # Load image
     p = pydicom.dcmread(DICOM_FILE_PATH)
@@ -70,22 +81,23 @@ def main():
 
     # Create plots
     fig, axes = plt.subplots(1, 2, figsize=(14, 7))    
-    # Left
     axes[0].imshow(image, cmap='gray')
     axes[0].imshow(overlay_muscle)
     axes[0].axis("off")
     axes[0].set_title('Muscle (red)')
-    # Right
     axes[1].imshow(image, cmap="gray")
     axes[1].imshow(overlay_myosteatosis)
     axes[1].axis("off")
     axes[1].set_title(f"Muscle low-RA (yellow) and high-RA (red)")
-    # Slider
-    slider_ax = fig.add_axes([0.15, 0.06, 0.7, 0.03])
-    slider = Slider(slider_ax, "Threshold", -29, 150, valinit=hu_lo)
-    slider.on_changed(on_change)
-    # Slider value text
-    slider_value = fig.text(0.5, 0.05, f"Low-RA threshold: {hu_lo:.1f}", ha="center", va="center", color='white')
+
+    # Create sliders for low and high thresholds
+    slider_lo_ax = fig.add_axes([0.15, 0.06, 0.7, 0.03])
+    slider_lo = Slider(slider_lo_ax, "Low-RA", HU_MIN, HU_MAX, valinit=hu_lo)
+    slider_lo.on_changed(on_change_lo)
+    slider_hi_ax = fig.add_axes([0.15, 0.03, 0.7, 0.03])
+    slider_hi = Slider(slider_hi_ax, "High-RA", HU_MIN, HU_MAX, valinit=hu_hi)
+    slider_hi.on_changed(on_change_hi)
+
     plt.show()
 
 
