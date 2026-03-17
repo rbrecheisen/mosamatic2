6fb81ce39cea2599f83f27a249ded1392bb227dd
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2026-03-17 10:47:06 +0100

    Saving work

--- mosamatic2/src/experiments/contrastsensitivity/lisa/analysis.py
+++ mosamatic2/src/experiments/contrastsensitivity/lisa/analysis.py
@@ -3,12 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import ttest_rel, linregress
 
-BC_SCORES_FILE_PATH = 'M:\\data\\contrastsensitivity\\lieke\\output\\defaultpipeline\\calculatescorestask\\bc_scores.csv'
+
+BC_SCORES_FILE_PATH = 'M:\\data\\contrastsensitivity\\lisavalidation\\output\\defaultpipeline\\calculatescorestask\\bc_scores.csv'
 COEFFICIENT_FILE_PATH = 'M:\\data\\contrastsensitivity\\lieke\\output\\coefficients.json'
 
-def load_data():
+
+# LOADING DATA
+
+def load_scores():
+    # Format: L3_MUMC-LIV-001_unenhanced.dcm
     df = pd.read_csv(BC_SCORES_FILE_PATH, sep=';')
-    df = df.assign(patient_id=df['file'].str.split('_').str[0])
+    df = df.assign(patient_id=df["file"].str.rsplit("_", n=1).str[0])
     return df
 
 def load_scores_unenhanced(df):
@@ -20,16 +25,28 @@ def load_scores_arterial(df):
 def load_scores_venous(df):
     return df[df["file"].str.contains("venous", case=False, na=False)].copy()
 
-def align_on_common_patients(*dfs):
-    common = set(dfs[0]["patient_id"])
-    for d in dfs[1:]:
-        common &= set(d["patient_id"])
-    return [d[d["patient_id"].isin(common)].copy() for d in dfs], common
+def load_coefficients():
+    with open(COEFFICIENT_FILE_PATH, 'r') as f:
+        return json.load(f)
+
+# STATISTICS
 
 def ttest(df1, df2, column):
     t_stat, p_value = ttest_rel(df1[column], df2[column], nan_policy='omit')
     print(f'[{column}] t = {t_stat:.3f}, p-value = {p_value:.3g}')
     return t_stat, p_value
+
+# TRANSFORM
+
+def transform_df_to_venous(df_old, coefficients):
+    df_new = df_old.copy()
+    for column in ['muscle_area', 'muscle_ra', 'vat_area', 'vat_ra', 'sat_area', 'sat_ra']:
+        slope = coefficients[column]['slope']
+        intercept = coefficients[column]['intercept']
+        df_new[column] = slope * df_new[column].to_numpy() + intercept
+    return df_new
+
+# DISPLAY
 
 def print_ttest_results(df_venous, df_unenhanced, df_arterial):
     print('\nUnenhanced vs. venous:')
@@ -104,67 +121,34 @@ def create_scatter_plots(df_venous, df_unenhanced, df_arterial):
     axes1[2, 1].set_title('SAT RA')
     plt.show()
 
-def create_regression_coefficients(df_venous, df_unenhanced, df_arterial):
-    coefficients = {
-        'unenhanced_to_venous': {
-            'muscle_area': {'slope': 0, 'intercept': 0},
-            'muscle_ra': {'slope': 0, 'intercept': 0},
-            'vat_area': {'slope': 0, 'intercept': 0},
-            'vat_ra': {'slope': 0, 'intercept': 0},
-            'sat_area': {'slope': 0, 'intercept': 0},
-            'sat_ra': {'slope': 0, 'intercept': 0},
-        },
-        'arterial_to_venous': {
-            'muscle_area': {'slope': 0, 'intercept': 0},
-            'muscle_ra': {'slope': 0, 'intercept': 0},
-            'vat_area': {'slope': 0, 'intercept': 0},
-            'vat_ra': {'slope': 0, 'intercept': 0},
-            'sat_area': {'slope': 0, 'intercept': 0},
-            'sat_ra': {'slope': 0, 'intercept': 0},
-        }
-    }
-    for column in ['muscle_area', 'muscle_ra', 'vat_area', 'vat_ra', 'sat_area', 'sat_ra']:
-        result = linregress(x=df_unenhanced[column].to_numpy(), y=df_venous[column].to_numpy())
-        coefficients['unenhanced_to_venous'][column]['slope'] = result.slope
-        coefficients['unenhanced_to_venous'][column]['intercept'] = result.intercept
-    for column in ['muscle_area', 'muscle_ra', 'vat_area', 'vat_ra', 'sat_area', 'sat_ra']:
-        result = linregress(x=df_arterial[column].to_numpy(), y=df_venous[column].to_numpy())
-        coefficients['arterial_to_venous'][column]['slope'] = result.slope
-        coefficients['arterial_to_venous'][column]['intercept'] = result.intercept
-    with open(COEFFICIENT_FILE_PATH, 'w') as f:
-        json.dump(coefficients, f, indent=4)
-    return coefficients
-
-def transform_df_to_venous(df_old, coefficients):
-    df_new = df_old.copy()
-    for column in ['muscle_area', 'muscle_ra', 'vat_area', 'vat_ra', 'sat_area', 'sat_ra']:
-        slope = coefficients[column]['slope']
-        intercept = coefficients[column]['intercept']
-        df_new[column] = slope * df_new[column].to_numpy() + intercept
-    return df_new
+# MAIN
 
 def main():
-    # Load BC scores, determine smallest df and align all dfs 
-    # on common patients
-    df = load_data()
-    df_venous = load_scores_venous(df)
-    df_smallest = df_venous
-    df_unenhanced = load_scores_unenhanced(df)
-    if df_unenhanced.shape[0] < df_smallest.shape[0]:
-        df_smallest = df_unenhanced
-    df_arterial = load_scores_arterial(df)
-    if df_arterial.shape[0] < df_smallest.shape[0]:
-        df_smallest = df_arterial
-    (dfs_aligned, common_ids) = align_on_common_patients(df_venous, df_unenhanced, df_arterial)
-    df_venous, df_unenhanced, df_arterial = dfs_aligned
+
+    # Load BC scores for unenhanced, arterial and venous phase images
+    scores = load_scores()
+    scores_unenhanced = load_scores_unenhanced(scores)
+    scores_arterial = load_scores_arterial(scores)
+    scores_venous = load_scores_venous(scores)
+
+    # Load regression coefficients
+    coefficients = load_coefficients()
+    coefficients_unenhanced = coefficients['unenhanced_to_venous']
+    coefficients_arterial = coefficients['arterial_to_venous']
+
+    # Plot raw scores and print t-test results before conversion
+    # create_scatter_plots(scores_venous, scores_unenhanced, scores_arterial)
     print_ttest_results(
-        df_venous, df_unenhanced, df_arterial)
-    coefficients = create_regression_coefficients(df_venous, df_unenhanced, df_arterial)
-    df_unenhanced_new = transform_df_to_venous(df_unenhanced, coefficients['unenhanced_to_venous'])
-    df_arterial_new = transform_df_to_venous(df_arterial, coefficients['arterial_to_venous'])
+        scores_venous, scores_unenhanced, scores_arterial)
+
+    # Convert unenhanced and arterial scores to venous
+    scores_unenhanced_converted = transform_df_to_venous(scores_unenhanced, coefficients_unenhanced)
+    scores_arterial_converted = transform_df_to_venous(scores_arterial, coefficients_arterial)
+
+    # Plot new scores and print t-test results
+    # create_scatter_plots(scores_venous, scores_unenhanced_converted, scores_arterial_converted)
     print_ttest_results(
-        df_venous, df_unenhanced_new, df_arterial_new)
-    # create_scatter_plots(df_venous, df_unenhanced, df_arterial)
+        scores_venous, scores_unenhanced_converted, scores_arterial_converted)
 
 if __name__ == '__main__':
     main()
