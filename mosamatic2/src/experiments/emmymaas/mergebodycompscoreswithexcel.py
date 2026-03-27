2c216fdd7b7b7ffb30dc141da3f203abc60cb6e7
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2026-03-27 13:20:57 +0100

    Saving work

---
+++ mosamatic2/src/experiments/emmymaas/mergebodycompscoreswithexcel.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+
+DF_SCORES_1 = "M:\\data\\emmymaas\\13-03-2026\\output\\defaultpipeline\\calculatescorestask\\bc_scores.xlsx"
+DF_SCORES_2_= "M:\\data\\emmymaas\\23-03-2026\\output\\defaultpipeline\\calculatescorestask\\bc_scores.xlsx"
+DF_EMMY = "M:\\data\\emmymaas\\Database.xlsx"
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
