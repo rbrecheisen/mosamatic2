bc79192e42886de9a448e7f55dd786d5e646b4ae
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2026-04-14 14:22:50 +0200

    Saving work

---
+++ mosamatic2/src/experiments/corpus/main.py
@@ -0,0 +1,12 @@
+import os
+
+DIR_ORIGINAL = 'M:\\data\\corpus\\08-04-2026\\original'
+DIR_ORIGINAL_FLATTENED = 'M:\\data\\corpus\\08-04-2026\\original_flattened'
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
